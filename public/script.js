064eca5465351ada199023563887aa4d3896c135
declanrjb <declanrjb@gmail.com>
2024-10-18 17:47:52 -0400

    update site

--- public/script.js
+++ public/script.js
@@ -1,6 +1,17 @@
 (() => {
   // <stdin>
   $(function() {
-    console.log("hello world");
+    $("#stories-more").on("click", function() {
+      $("#posts-redacted").css("display", "none");
+      $("#posts-full").css("display", "block");
+      $("#stories-more").css("display", "none");
+      $("#stories-less").css("display", "block");
+    });
+    $("#stories-less").on("click", function() {
+      $("#posts-redacted").css("display", "block");
+      $("#posts-full").css("display", "none");
+      $("#stories-more").css("display", "block");
+      $("#stories-less").css("display", "none");
+    });
   });
 })();
